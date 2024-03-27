9f0b7b994eecc54dc1eb996510a9b12a0deff4b0
umamanasa <umamanasa49@gmail.com>
2024-03-27 15:21:53 +0530

    peering

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ module "vpc" {
   source   = "git::https://github.com/umamanasa/expense-module-vpc.git"
 
   for_each = var.vpc
-  cidr = each.value["cidr"]
-  subnets = each.value["subnets"]
-  default_vpc_id = var.default_vpc_id
+  cidr                        = each.value["cidr"]
+  subnets                     = each.value["subnets"]
+  default_vpc_id              = var.default_vpc_id
+  default_vpc_cidr            = var.default_vpc_cidr
+  default_vpc_route_table_id  = var.default_vpc_route_table_id
 }
 
 output "vpc" {
