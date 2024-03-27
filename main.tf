8feab5cb5fc7efceae928c2da63272c83e2130ec
umamanasa <umamanasa49@gmail.com>
2024-03-27 11:30:22 +0530

    igw creation

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-module "components" {
+module "vpc" {
   source   = "git::https://github.com/umamanasa/expense-module-vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
 
+}
+
+output "vpc" {
+  value = module.vpc
 }
