f37f5c4ed6422975186611f93407daf6ae113c94
umamanasa <umamanasa49@gmail.com>
2024-05-01 18:39:06 +0530

    helm charts deploy

--- main.tf
+++ main.tf
@@ -91,43 +91,54 @@ resource "aws_instance" "load_runner" {
   }
 }
 
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 19.0"
-#
-#   cluster_name    = "prod-expense"
-#   cluster_version = "1.28"
-#
-#   cluster_endpoint_public_access  = false
-#
-#   cluster_addons = {
-#     coredns = {
-#       most_recent = true
-#     }
-#     kube-proxy = {
-#       most_recent = true
-#     }
-#     vpc-cni = {
-#       most_recent = true
-#     }
-#   }
-#
-#   vpc_id                   = local.vpc_id
-#   subnet_ids               = local.app_subnets
-#   control_plane_subnet_ids = local.app_subnets
-#
-#
-#   eks_managed_node_groups = {
-#     green = {
-#       min_size     = 1
-#       max_size     = 10
-#       desired_size = 3
-#
-#       instance_types = ["t3.large"]
-#       capacity_type  = "SPOT"
-#     }
-#   }
-#
-#   tags = var.tags
-# }
+## FOR KUBERNETES DEPLOYMENT - EKS
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "prod-expense"
+  cluster_version = "1.28"
+
+  cluster_endpoint_public_access  = false
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.app_subnets
+  control_plane_subnet_ids = local.app_subnets
+
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 3
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_security_group_rule" "https-to-eks" {
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = module.eks.cluster_security_group_id
+  to_port           = 443
+  type              = "ingress"
+  cidr_blocks       = var.ssh_ingress_cidr
+}
